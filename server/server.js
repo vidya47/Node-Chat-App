e10f43b19404631a4fe2b803567ea5d842bc48f4
vidya47 <vidya47@gmail.com>
2018-03-04 22:20:01 -0500

    Emit newMessage and createMessage

--- server/server.js
+++ server/server.js
@@ -14,19 +14,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newEmail', {
-    from: 'vijaycrvi@gmail.com',
-    text: 'Hey. What is going on. ',
-    createAt: 123
-  });
-
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
-  socket.on('createEmail', (newEmail) => {
-    console.log('createEmail', newEmail);
-  });
 
   socket.on('disconnect', ()=>{
     console.log('User Disconnected');
